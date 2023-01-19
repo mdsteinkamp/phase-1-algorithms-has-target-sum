f56c0beb0977cb2ff5b1498bfa2646c22298a84b
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-01-18 16:35:15 -0800

    commit tree 94844ffc8c6a43ea1b50e42b0d17792a71b4af7c

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+function hasTargetSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    const complementNum = target - arr[i];
+      for (let j = i+ 1; j < arr.length; j++) {
+        if (arr[j] === complementNum) return true
+      }
+    }
+    return false
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  
 */
 
 /* 
-  Add your pseudocode here
-*/
+Write function that takes in the array & target as arguments
+    iterate over the array
+      for current index, determine the complement number that adds to the target
+      iterate over the remaining numbers in the array
+        if any of the remaining numbers is the complement number return true
+    Return false if reach end of the array and complement number is not found*/
 
 /*
   Add written explanation of your solution here
+  
+
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +41,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([5, 6, 80, -7, 0, 0], 0));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([25, 30, -25, 75], 50));
 }
 
 module.exports = hasTargetSum;
